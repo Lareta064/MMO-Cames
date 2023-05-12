f0e43e3cf3d221847b83ecb57da20221035badfe
Lareta064 <lareta064@gmail.com>
2023-05-12 08:53:23 +0300

    fix

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,44 @@
 document.addEventListener("DOMContentLoaded", function (){        
 
+	/*========= HEADER MENU MOBILE ========= */
+	const menuToggle = document.querySelector('.toggle-menu');
+	const mobMenu = document.querySelector('.header-menu');
+	const overlayBlock = document.querySelector('#overlay');
+	// const backTopButton = document.querySelector('#back-top');
+
+	const bodyEl = document.body;
+	if (menuToggle) {
+		menuToggle.addEventListener('click', function () {
+			if (this.classList.contains('active')) {
+				this.classList.remove('active');
+				mobMenu.classList.remove('active');
+				overlayBlock.classList.remove('active');
+				bodyEl.classList.remove('lock');
+
+			} else {
+				this.classList.add('active');
+				mobMenu.classList.add('active');
+				overlayBlock.classList.add('active');
+				bodyEl.classList.add('lock');
+
+			}
+		});
+		window.addEventListener('resize', function () {
+			menuToggle.classList.remove('active');
+			overlayBlock.classList.remove('active');
+			bodyEl.classList.remove('lock');
+			mobMenu.classList.remove('active');
+
+		});
+		mobMenu.addEventListener('click', function () {
+			this.classList.remove('active');
+			menuToggle.classList.remove('active');
+			overlayBlock.classList.remove('active');
+			bodyEl.classList.remove('lock');
+
+		});
+	}
+	
 	/*=========RATING BLOCK =======*/
 	const ratingBlock = document.querySelectorAll('[data-rating]');
 	if(ratingBlock){
